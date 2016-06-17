b1b15073cb41633d0656bae40a1261b05418990f
Daniel Furze <daniel@furzeface.com>
2016-06-17 10:32:37 +0100

    feat: add heroku-toolbelt to brew

--- .brew.sh
+++ .brew.sh
@@ -6,20 +6,17 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sudo xcodebuild -license
 
 # Install Git
-echo Installing Git…
 brew install git
 brew install git-flow
 brew install git-lfs
 
 # Install Node.js - this installs `npm` too, using the recommended installation method
-echo Installing Node…
 brew install node
 
-echo Installing Imagemagick…
 brew install imagemagick
+brew install heroku-toolbelt
 
 # Install Brew Cask for managing other apps
-echo Installing Brew Cask…
 brew install caskroom/cask/brew-cask
 
 # Install Cask versions for beta version of apps :D
