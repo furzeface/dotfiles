11952a28ee7ee3c3c363fa1f6dd8b1e3f8fe7ad6
Daniel Furze <dan@danfurze.com>
2019-10-30 07:53:54 +0000

    updaetssses

--- .brew.sh
+++ .brew.sh
@@ -14,20 +14,18 @@ brew install git-secrets
 
 # Install Node.js - this installs `npm` too, using the recommended installation method
 brew install node
-
 brew install nvm
 
-# Install Yarn cos Yarn’s the cool new thing
 brew install yarn --without-node
 
 # Wget is the best
 brew install wget
 
+
 # Install Brew Cask for managing other apps
 brew install caskroom/cask/brew-cask
-
-# Install Cask versions for beta version of apps :D
 brew tap caskroom/versions
+brew tap homebrew/cask-fonts
 
 # Remove outdated versions from the cellar
 brew cleanup
